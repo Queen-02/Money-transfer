498a2b12afc40a0a2a44775e610f08a86cb9dc8a
Queen-02 <queenzyblack176@gmail.com>
2022-05-10 21:43:30 +0300

    Initialized a button

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -7,15 +7,22 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnBMI : Button
+    lateinit var money: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnBMI = findViewById(R.id.btnBMI)
+        money = findViewById(R.id.btnMoney)
 
         btnBMI.setOnClickListener {
             val intent = Intent(this, BMI_Calcualtor::class.java)
+            startActivity(intent)
+        }
+
+        money.setOnClickListener {
+            val intent = Intent(this, MoneyTransfer::class.java)
             startActivity(intent)
         }
     }
